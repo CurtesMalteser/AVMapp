9858516283f47df8c1870aa0a34d3d0589c97bf6
curtes_malteser <antonio_bastiao@hotmail.com>
2019-11-16 01:35:40 +0100

    initial commit and added first two screens

---
+++ lib/secondscreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AVM Rocks',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: SecondPage(title: 'AVM Home Page'),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  SecondPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff1e537b),
+      appBar: AppBar(
+        title: Text("Second Route AVM"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyApp()),
+            );
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
